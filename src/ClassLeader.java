54daafbfa4380fbfa74c25db0d4526ad7a892b1c
mchingis <53611127+MChingS@users.noreply.github.com>
2021-07-26 22:14:26 +0200

    OOP

--- src/ClassLeader.java
+++ src/ClassLeader.java
@@ -5,7 +5,7 @@ public class ClassLeader extends Student{ //IS A relationship
     public void monitorClass() {
         System.out.println("Monitor method in class-leader class");
     }
-    @Override //poly
+    @Override //polymorphism
     public void learn() {
         System.out.println("Common method in class-leader which is Learning method");
 
